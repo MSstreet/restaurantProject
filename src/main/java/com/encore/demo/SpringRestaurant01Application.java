82c9d1275757be5d37656580e2d068f78a2e4050
mason <cortm5pp@gmail.com>
2022-03-18 15:19:09 +0900

    message

---
+++ src/main/java/com/encore/demo/SpringRestaurant01Application.java
@@ -0,0 +1,13 @@
+package com.encore.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestaurant01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestaurant01Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/encore/demo/vo/Member.java
@@ -0,0 +1,38 @@
+package com.encore.demo.vo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString
+public class Member {
+	
+	@Id
+	private String id;
+	
+	@Column(nullable=false)
+	private String pwd;
+	
+	@Column(nullable=false)
+	private String pwd_check;
+	
+	@Column(nullable=false)
+	private String name;
+	
+	@Column(nullable=false)
+	private String tel;
+	
+	@Column(nullable=false)
+	private String email;
+	
+	@Column(nullable=false)
+	private int type;// 1:�쑀��, 2:�젏二�
+
+}
